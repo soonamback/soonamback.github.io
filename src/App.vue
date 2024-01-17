d367eadd3eeeae8546d5f6a75c449ae233e87b1f
soonamback <soonam.yi123@gmail.com>
2024-01-17 13:07:41 +0900

    add layout

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ <TheTwoColumnsLayout :leftColumnClass="'col-md-8 bg-vue'" :rightColumnClass="'col-md-4'" :fullsize="true"/>
+</template>
+
+<script>
+import TheTwoColumnsLayout from './layouts/TheTwoColumnsLayout'
+
+
+export default {
+  name: 'App',
+  components: {
+    TheTwoColumnsLayout,
+  }
+}
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+@import "~animate.css/animate.min.css";
+
+.bg-vue {
+  background-color: rgb(52, 73, 94);
+  color: white;
+}
+.bg-vue2 {
+  background-color: rgb(65, 184, 131);
+  color: white;
+}
+.text-vue {
+  color: rgb(52, 73, 94);
+}
+.text-vue2 {
+  color: rgb(65, 184, 131);
+}
+</style>
---
+++ src/layouts/TheTwoColumnsLayout.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container-fluid" :class="containerClasses">
+      <div class="row" :class="rowClasses">
+        <div class="col-md-8" :class="leftColumnClasses">
+          <slot name="leftCol">
+            <h1>left Col</h1>
+          </slot>
+        </div>
+        <div class="col-md-4" :class="rightColumnClasses">
+          <slot name="rightCol">
+            <h1>RIGHT Col</h1>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "TheTwoColumnsLayout",
+    props: {
+      leftColumnClass: {
+        type: String,
+        default: "col-md-8",
+      },
+      rightColumnClass: {
+        type: String,
+        default: "col-md-4",
+      },
+      fullsize: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    computed: {
+      leftColumnClasses() {
+        return [this.leftColumnClass, this.fullsize ? "h-100" : ""];
+      },
+      rightColumnClasses() {
+        return [this.rightColumnClass, this.fullsize ? "h-100" : ""];
+      },
+      rowClasses() {
+        return [this.fullsize ? "h-100" : ""];
+      },
+      containerClasses() {
+        return [this.fullsize ? "vh-100" : ""];
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
