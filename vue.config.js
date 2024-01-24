e0a768ffa93cd112738ec434ac7c42a28aaf3581
soonamback <soonam.yi123@gmail.com>
2024-01-24 11:58:30 +0900

    change vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-})
+const path = require("path");
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-shop/" : "/",
+}
