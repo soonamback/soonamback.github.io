8b2652450a5c9fcf8771b5b44a3827ec5cfe9c87
soonamback <soonam.yi123@gmail.com>
2024-01-24 16:17:46 +0900

    add browserlist

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -1,4 +1,4 @@
-import { FIREBASE_API_KEY } from "../../../config/firebase.js";
+import { firebase_api_key } from "../../../config/firebase.js";
 import axios from 'axios'
 
 let timer;
@@ -17,9 +17,9 @@ const actions = {
         auth(context, payload) {
             let url = "";
             if(payload.mode === "signin"){
-                url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`
+                url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebase_api_key}`
             } else if (payload.mode === "signup") {
-               url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FIREBASE_API_KEY}`
+               url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebase_api_key}`
             } else {
                 return;
             }
